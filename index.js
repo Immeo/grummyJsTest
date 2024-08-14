cc907cb71d461f0f2b19fd1d7ab686c6a54458bd
immeo <wayfarer245@gmail.com>
2024-08-15 00:47:22 +0300

    02-add error catch

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Bot } = require('grammy');
+const { Bot, GrammyError, HttpError } = require('grammy');
 
 const bot = new Bot(process.env.KEY);
 
@@ -9,6 +9,19 @@ bot.command('start', ctx => {
 
 bot.on('message', ctx => {
 	ctx.reply('You said: ' + ctx.message.text);
+});
+
+bot.catch(err => {
+	const ctx = err.ctx;
+	console.error(`Error while handling update ${ctx.update.update_id}:`);
+	const e = err.error;
+	if (e instanceof GrammyError) {
+		console.error('Error in request:', e.description);
+	} else if (e instanceof HttpError) {
+		console.error('Could not contact Telegram:', e);
+	} else {
+		console.error('Unknown error:', e);
+	}
 });
 
 bot.start();
