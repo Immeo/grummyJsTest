56f455fc90dcef8344dd3d19d4a08ce902d6a950
immeo <wayfarer245@gmail.com>
2024-08-20 15:30:43 +0300

    08.1-add example spoiler

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ bot.api.setMyCommands([
 // dont used camalCase because it call error, please use snake_case
 bot.command('start', ctx => {
 	ctx.reply(
-		`Hello ${ctx.from.first_name}! Github author bot <a href="https://github.com/Immeo">link</a>`,
+		`Hello ${ctx.from.first_name}! Github author bot <a href="https://github.com/Immeo"><span class="tg-spoiler">link</span></a>`,
 		{ parse_mode: 'HTML' }
 	);
 });
